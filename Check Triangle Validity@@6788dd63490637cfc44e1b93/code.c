bcfa8aec0546a9909d7b9a88724b8b70e6b5b9f3
a-gk-190 <gargakshit190@gmail.com>
2025-01-31 20:51:57 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    // printf("%s", welcome());
+    int side1,side2,side3;
+    scanf("%d %d %d",&side1,&side2,&side3);
+    if( (side2+side3)>side1 && (side1+side2)>side3 && (side1+side3)>side2){
+        printf("Valid");
+    }else{
+        printf("Invalid");
+    }
+    return 0;
+}
