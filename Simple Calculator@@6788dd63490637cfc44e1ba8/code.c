01c2cd39893b0f876d9e4dd9d038eeb3f8609343
a-gk-190 <gargakshit190@gmail.com>
2025-02-03 20:47:24 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,20 +4,21 @@ char* welcome() {
     return "Welcome to Code Radar!";
 }
 
-int main() {
+int main() 
+{
     // printf("%s", welcome());
-    int num1,num2;
+    double num1,num2;
     scanf("%d %d",&num1,&num2);
     char Operator;
     scanf("%c",&Operator);
     if(Operator == '+'){
-        printf(num1+num2);
+        printf("%d",num1+num2);
     }else if(Operator == "-"){
-        printf(num1 - num2);
+        printf("%d",num1 - num2);
     }else if(Operator == "*"){
-        printf(num1*num2);
+        printf("%d",num1*num2);
     }else if(Operator == "/"){
-        printf(num1/num2);
+        printf("%d",num1/num2);
     }else{
         printf("Error");
     }
