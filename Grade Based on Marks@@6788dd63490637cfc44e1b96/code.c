7084cafca4d1b76122019d1da6ec1d137675ea19
a-gk-190 <gargakshit190@gmail.com>
2025-02-03 17:12:24 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    // printf("%s", welcome());
+    int num;
+    scanf("%d",&num);
+    if(num>=90){
+        printf("A");
+    }else if(num>=80 ){
+printf("B");
+    }else if(num>=70){
+printf("C");
+    }else if(num>=60){
+        printf("D");
+    }else {
+        printf("Fail");
+    }
+    return 0;
+}
