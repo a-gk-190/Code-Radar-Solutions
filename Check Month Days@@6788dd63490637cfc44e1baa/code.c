78f14d61301c6ad9e9608fd41054b7de43f45d7f
a-gk-190 <gargakshit190@gmail.com>
2025-02-04 16:20:54 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -9,29 +9,29 @@ int main() {
     int num;
     scanf("%d",&num);
     if(num==1){
-        printf(31);
+        printf("%d",31);
     }else if(num==2){
-        printf(28);
+        printf("%d",28);
     }else if(num==3){
-        printf(31);
+        printf("%d",31);
     }else if(num==4){
-        printf(30);
+        printf("%d",30);
     }else if(num==5){
-        printf(31);
+        printf("%d",31);
     }else if(num==6){
-        printf(30);
+        printf("%d",30);
     }else if(num==7){
-        printf(31);
+        printf("%d",31);
     }else if(num==8){
-        printf(31);
+        printf("%d",31);
     }else if(num==9){
-        printf(30);
+        printf("%d",30);
     }else if(num==10){
-        printf(31);
+        printf("%d",31);
     }else if(num==11){
-        printf(30);
+        printf("%d",30);
     }else if(num==12){
-        printf(31);
+        printf("%d",31);
     }else{
         printf("Invalid month");
     }
