03c46d5fff35d54d752e43c734b474736364c201
a-gk-190 <gargakshit190@gmail.com>
2025-02-04 17:32:34 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,21 +5,45 @@ char* welcome() {
 }
 
 int main() {
-    // printf("%s", welcome());
-    int num1,num2;
-    char ch;
-    scanf("%d %d",&num1,&num2);
-    scanf("%c",&ch);
-    if(ch=='+'){
-        printf("%d",num1+num2);
-    }else if(ch=='-'){
-        printf("%d",num1-num2);
-    }else if(ch=='*' ){
-        printf("%d",num1*num2);
-    }else if(ch=='/' && num2!=0){
-        printf("%d",num1/num2);
-    }else {
-        printf("error");
+    
+    // int num1,num2;
+    // char ch;
+    // scanf("%d %d",&num1,&num2);
+    // scanf("%c",&ch);
+    // if(ch=='+'){
+        
+    //     printf("%d",num1+num2);
+    // }else if(ch=='-'){
+    //     printf("%d",num1-num2);
+    // }else if(ch=='*' ){
+    //     printf("%d",num1*num2);
+    // }else if(ch=='/' && num2!=0){
+    //     printf("%d",num1/num2);
+    // }else {
+    //     printf("error");
+    // }
+    int a,b,div,add,sub,multi;
+    char c;
+    scanf("%d%d",&a,&b);
+    add=a+b;
+    sub=a-b;
+    multi=a*b;
+    div=a/b;
+    scanf("%d%d%d%d",&add,&multi,&sub,&div);
+    if(c=="+"){
+        printf("%d",add);
     }
+    else if(c=="-"){
+        printf("%d",,sub);
+    }
+    else if(c=="*"){
+        printf("%d",multi);
+    }
+    else if(c=="/"){
+        printf("%d",div);
+    }
+
+
+
     return 0;
 }
